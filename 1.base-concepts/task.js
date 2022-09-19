295460ad2bd3e6c4ac8e101fbc0258c3ee93da71
Родионов Алексей Игоревич <rodionov_a@tii.ru>
2022-09-19 17:41:02 +0300

    resolve task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,48 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let discriminant = Math.pow(b, 2) - 4 * a * c,
+        arr = [];
+    if (discriminant === 0) {
+        arr.push(-b / (2 * a));
+    } else if (discriminant > 0) {
+        arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)]
+    }
+    // код для задачи №1 писать здесь
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let percentPerMonth,
+        contributionNew,
+        amountNew;
 
-  // код для задачи №2 писать здесь
+    if (parseFloat(percent)) {
+        percentPerMonth = parseFloat(percent / 100 / 12);
+    } else {
+        return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    }
 
-  return totalAmount;
+    if (parseInt(contribution) || contribution == 0) {
+        contributionNew = (parseInt(contribution)) ? parseInt(contribution) : 0;
+    } else {
+        return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    }
+
+    if (parseInt(amount)) {
+        amountNew = parseInt(amount);
+    } else {
+        return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    }
+
+
+    let today = new Date(),
+        dateX = new Date(date),
+        months = parseInt((dateX - today) / (1000 * 3600 * 24 * 30)),
+        payToBank = amountNew - contributionNew,
+        paymentPerMonth = payToBank * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth) ** months) - 1)),
+        totalAmount = paymentPerMonth * months;
+
+
+    return parseFloat(totalAmount.toFixed(2));
 }
